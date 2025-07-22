62aca2248bd324c473c840a5b7b23b313b02afac
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2025-07-22 08:15:49 -0300

    domain events

---
+++ Nacoes.Agendamentos.Infra/DomainEvents/IDomainEventsDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using Nacoes.Agendamentos.Domain.Abstracts;
+
+namespace Nacoes.Agendamentos.Infra.DomainEvents;
+
+public interface IDomainEventsDispatcher
+{
+    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+}
