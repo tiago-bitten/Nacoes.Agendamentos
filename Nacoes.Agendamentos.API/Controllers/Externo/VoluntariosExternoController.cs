561b5ca23fe88943260f19760dfb63995a999660
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2025-07-22 22:34:05 -0300

    some fixes

--- Nacoes.Agendamentos.API/Controllers/Externo/VoluntariosExternoController.cs
+++ Nacoes.Agendamentos.API/Controllers/Externo/VoluntariosExternoController.cs
@@ -3,12 +3,14 @@ using Nacoes.Agendamentos.API.Controllers.Abstracts;
 using Nacoes.Agendamentos.API.Extensions;
 using Nacoes.Agendamentos.Application.Abstracts.Messaging;
 using Nacoes.Agendamentos.Application.Authentication.Context;
+using Nacoes.Agendamentos.Application.Common.Filters;
 using Nacoes.Agendamentos.Application.Entities.Voluntarios.Commands.Adicionar;
 using Nacoes.Agendamentos.Domain.Entities.Voluntarios;
 using Nacoes.Agendamentos.Domain.ValueObjects;
 
 namespace Nacoes.Agendamentos.API.Controllers.Externo;
 
+[VoluntarioAuthorization]
 [Route("api/voluntarios-externo")]
 public sealed class VoluntariosExternoController : NacoesController
 {
@@ -18,7 +20,6 @@ public sealed class VoluntariosExternoController : NacoesController
                                                [FromServices] IAmbienteContext ambienteContext,
                                                [FromBody] AdicionarVoluntarioCommand command)
     {
-        ambienteContext.StartBotSession();
         var result = await handler.Handle(command);
 
         return result.AsHttpResult(mensagem: "Cadastro realizado com sucesso.");
--- Nacoes.Agendamentos.Application/Common/Filters/VoluntarioAuthorizationAttribute.cs
+++ Nacoes.Agendamentos.Application/Common/Filters/VoluntarioAuthorizationAttribute.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using Nacoes.Agendamentos.Application.Authentication.Context;
+using Nacoes.Agendamentos.Application.Common.Responses;
 using Nacoes.Agendamentos.Application.Entities.Voluntarios.Interfaces;
+using Nacoes.Agendamentos.Domain.Common;
 
 namespace Nacoes.Agendamentos.Application.Common.Filters;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public sealed class VoluntarioAutenticadoAttribute : Attribute, IAsyncActionFilter
+public sealed class VoluntarioAuthorizationAttribute : Attribute, IAsyncActionFilter
 {
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
@@ -16,6 +19,9 @@ public sealed class VoluntarioAutenticadoAttribute : Attribute, IAsyncActionFilt
             !httpContext.Request.Headers.TryGetValue("data-nascimento", out var dataNascimentoStr))
         {
             httpContext.Response.StatusCode = 401;
+            httpContext.Response.ContentType = "application/json";
+            var error = new Error("Voluntario.Login", ErrorType.Unauthorized, "Cpf e/ou data de nascimento não informados.");
+            await httpContext.Response.WriteAsJsonAsync(ApiResponse.Erro(error));
             return;
         }
 
@@ -28,6 +34,9 @@ public sealed class VoluntarioAutenticadoAttribute : Attribute, IAsyncActionFilt
         if (loginDto is null)
         {
             httpContext.Response.StatusCode = 401;
+            httpContext.Response.ContentType = "application/json";
+            var error = new Error("Voluntario.Login", ErrorType.Unauthorized, "Não foi possível localizar seu cadastro.");
+            await httpContext.Response.WriteAsJsonAsync(ApiResponse.Erro(error));
             return;
         }
 
