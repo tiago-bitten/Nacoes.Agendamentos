86dcac9fe39392acd0c6d6fe8417abeef0355982
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2025-07-26 18:46:37 -0300

    continue

---
+++ Nacoes.Agendamentos.Application/Entities/Agendas/Commands/AdicionarAgendamento/AdicionarAgendamentoCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Nacoes.Agendamentos.Application.Entities.Agendas.Commands.AdicionarAgendamento;
+
+internal sealed class AdicionarAgendamentoCommandValidator : AbstractValidator<AdicionarAgendamentoCommand>
+{
+    public AdicionarAgendamentoCommandValidator()
+    {
+        RuleFor(x => x.VoluntarioMinisterioId).NotEmpty();
+        RuleFor(x => x.AtividadeId).NotEmpty();
+    }
+}
---
+++ Nacoes.Agendamentos.Application/Entities/Agendas/Commands/AdicionarAgendamento/AdicionarAgendamentoHandler.cs
@@ -0,0 +1,21 @@
+using Nacoes.Agendamentos.Application.Abstracts.Messaging;
+using Nacoes.Agendamentos.Domain.Abstracts.Interfaces;
+using Nacoes.Agendamentos.Domain.Common;
+using Nacoes.Agendamentos.Domain.Entities.Agendas.Interfaces;
+using Nacoes.Agendamentos.Domain.Entities.Ministerios.Interfaces;
+using Nacoes.Agendamentos.Domain.Entities.Voluntarios.Interfaces;
+using AgendamentoId = Nacoes.Agendamentos.Domain.ValueObjects.Id<Nacoes.Agendamentos.Domain.Entities.Agendas.Agendamento>;
+
+namespace Nacoes.Agendamentos.Application.Entities.Agendas.Commands.AdicionarAgendamento;
+
+internal sealed class AdicionarAgendamentoHandler(IUnitOfWork uow,
+                                                  IAgendaRepository agendaRepository,
+                                                  IVoluntarioMinisterioRepository voluntarioMinisterioRepository,
+                                                  IAtividadeRepository atividadeRepository)
+    : ICommandHandler<AdicionarAgendamentoCommand, AgendamentoId>
+{
+    public async Task<Result<AgendamentoId>> Handle(AdicionarAgendamentoCommand command, CancellationToken cancellation = default)
+    {
+        throw new NotImplementedException();
+    }
+}
